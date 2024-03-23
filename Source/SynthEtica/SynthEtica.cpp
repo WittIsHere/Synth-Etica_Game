38207a2de6311d295dd1fae8aeca119c3bb3c3e1
WittIsHere <59049883+WittIsHere@users.noreply.github.com>
2024-03-24 00:30:41 +0100

    UE_StarterProj
    Empty project, mobile, c++ (& bps).
    Want to also try Copilot.

---
+++ Source/SynthEtica/SynthEtica.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SynthEtica.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SynthEtica, "SynthEtica" );
